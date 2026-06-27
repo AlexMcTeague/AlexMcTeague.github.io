799b021d80499baa7768cdfc353f0410bfab406b
Flarkinater <mcteaguealex@gmail.com>
2026-06-27 11:53:25 -0400

    Update resume project descriptions by fetching repo descriptions

--- fetchProjects.js
+++ fetchProjects.js
@@ -4,14 +4,22 @@ const username = 'AlexMcTeague';
 // This script expects an "experience" class div, and a repo with a matching ID
 const projects = ['KMZ-Processor', 'Network-Design-Tools', 'DesignShark', 'RDOF-QC-Tool'];
 
-for (const project of projects) {
-    fetch(`https://api.github.com/repos/${username}/${project}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => console.log(data.description))
-        .catch(error => console.error('There was a problem with your fetch operation:', error));
+for (const projectId of projects) {
+    const node = document.querySelector(`#${projectId}.experience p`);
+    if (node == null) {
+        console.error(`Could not locate an html element associated with project ID: ${projectId}`);
+        continue;
+    }
+
+    fetch(`https://api.github.com/repos/${username}/${projectId}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        node.textContent = data.description;
+    })
+    .catch(error => console.error(`There was a problem while fetching project ${projectId}:`, error));
 }
